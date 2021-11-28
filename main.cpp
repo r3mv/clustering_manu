5f29ba96cbdae6d94410c80af1f6876185e1fb07
Remi VIEUX <remi.vieux@gmail.com>
2021-11-28 18:17:23 +0100

    first implem of import code

--- main.cpp
+++ main.cpp
@@ -1,13 +1,43 @@
 #include <iostream>
+#include <sstream>
 #include <fstream>
 #include <vector>
 
-const std::string filename = "data.csv";
+const std::string DATA_FILE = "data.csv";
 const int MAX_CLUSTER_SIZE = 26;
 const char SEPARATOR = ';';
   
 
+void
+importData(const std::string &filename,
+	   std::vector<double> &data,
+	   int &numRows,
+	   int &numCols) {
 
+  numCols = 2;
+  numRows = 0;
+  std::string buffer;
+  std::ifstream infile(filename);
+  while (infile.good()) {
+    infile >> buffer;
+    std::stringstream ss(buffer);
+    std::vector<string> split;
+    std::string val;
+    while (std::getline(ss, val, SEPARATOR)) {
+      split.push_back(val);
+    }
+    if (!split.size == 18) {
+      throw std::runtime_error("Expected 18 values");
+    } else {
+      double lat = std::atoi(split[16]);
+      double lon = std::atoi(split[17]);
+      data.push_back(lat);
+      data.push_back(lon);
+      numRows++;
+    }
+  }
+  infile.close();
+}
 
 
 int
@@ -16,7 +46,7 @@ main(int argc,char **argv){
   int numRows = -1;
   int numCols = -1;
   std::vector<double> data;
-  importData(data, filename, numRows, numCols);
+  importData(DATA_FILE, data, numRows, numCols);
 
   std::vector<double> distances;
   computeDistances(data, numRows, numCols);
