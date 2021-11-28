108fb46b333ad3c75a049f12a29adf836f222c06
Remi VIEUX <remi.vieux@gmail.com>
2021-11-28 17:57:38 +0100

    upd:Coded structure of main function

--- main.cpp
+++ main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+
+const std::string filename = "data.csv";
+const int MAX_CLUSTER_SIZE = 26;
+const char SEPARATOR = ';';
+  
+
+
+
 
 int
 main(int argc,char **argv){
 
-  
+  int numRows = -1;
+  int numCols = -1;
+  std::vector<double> data;
+  importData(data, filename, numRows, numCols);
+
+  std::vector<double> distances;
+  computeDistances(data, numRows, numCols);
+
+  std::vector<int> clusters(numRows);
+  for (int i = 0;i < numRows;++i){
+    clusters[i] = i;
+  }
+
+  computeClustering(distances, clusters, MAX_CLUSTER_SIZE);
+
+  return 0;
 
 }
