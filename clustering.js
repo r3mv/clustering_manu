9d40601a42f4b3ebf75216dac1c5a16ebc0eb57d
Remi VIEUX <remi.vieux@gmail.com>
2021-12-01 16:34:42 +0100

    upd:frontend/Calling clustering on submit form parameters ok

--- clustering.js
+++ clustering.js
@@ -1,8 +1,7 @@
-function callClustering(min, max, w1, w2, w3) : {
-    console.log("Calling callClustering");
-    // console.log("Calling callClustering with min=" + min
-    // 		+ " max=" + max
-    // 		+ " w1=" + w1
-    // 		+ " w2=" + w2
-    // 		+ " w3=" + w3);
+function callClustering(min, max, w1, w2, w3)  {
+    console.log("Calling callClustering with min=" + min
+		+ " max=" + max
+		+ " w1=" + w1
+		+ " w2=" + w2
+		+ " w3=" + w3);
 }
